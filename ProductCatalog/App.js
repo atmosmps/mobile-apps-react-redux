ab13e46d8d5c4ed4da431f46f32cebbbfdbf7dd6
atmosmaciel <atmos.mps@gmail.com>
2018-10-21 17:12:33 -0300

    Fisrt commit Product Catalog

---
+++ ProductCatalog/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import ItensList from './src/components/ItensList';
+
+class App extends Component {
+  render() {
+    return (
+      <ItensList/>
+    );
+  }
+}
