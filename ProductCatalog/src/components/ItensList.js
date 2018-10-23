e3723583fc19be276afbcf56961d4226ff2767af
atmosmaciel <atmos.mps@gmail.com>
2018-10-23 20:48:04 -0300

    Pegando dados da consulta da API

--- ProductCatalog/src/components/ItensList.js
+++ ProductCatalog/src/components/ItensList.js
@@ -5,18 +5,25 @@ import Itens from './Itens';
 
 export default class ItensList extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {itensList: []}
+  }
+
   componentWillMount() {
     axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
-    .then(response => {console.log(response)})
+    .then(response => {
+      this.setState({
+        itensList: response.data
+      })
+    })
     .catch(() => {console.log('Erro ao recuperar os dados.')}) // Apos a execucao da requisição eu possa na sequencia executar uma funcao recebendo por parametro uma resposta(response) e fazendo alguma coisa com o resultado.
   }
 
   render() {
     return (
       <View>
-        <Itens/>
-        <Itens/>
-        <Itens/>
+        {console.log(this.state.ItensList)}
       </View>
     );
   }
