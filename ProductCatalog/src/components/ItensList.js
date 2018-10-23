1e8cda01c9515db2ae49ca62de21df704ef882f9
atmosmaciel <atmos.mps@gmail.com>
2018-10-23 20:41:02 -0300

    Adicionando funcao com requisicao

--- ProductCatalog/src/components/ItensList.js
+++ ProductCatalog/src/components/ItensList.js
@@ -4,6 +4,13 @@ import axios from 'axios';
 import Itens from './Itens';
 
 export default class ItensList extends Component {
+
+  componentWillMount() {
+    axios.get('http://faus.com.br/recursos/c/dmairr/api/itens.html')
+    .then(response => {console.log(response)})
+    .catch(() => {console.log('Erro ao recuperar os dados.')}) // Apos a execucao da requisição eu possa na sequencia executar uma funcao recebendo por parametro uma resposta(response) e fazendo alguma coisa com o resultado.
+  }
+
   render() {
     return (
       <View>
