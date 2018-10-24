fc7933df0c10157f7663e1ccb5be568c9679a03b
atmosmaciel <atmos.mps@gmail.com>
2018-10-24 10:22:32 -0300

    .gitignore fix

--- ProductCatalog/index.js
+++
@@ -1,9 +0,0 @@
-/** @format */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import ItensList from './src/components/ItensList';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
-AppRegistry.registerComponent(appName, () => ItensList);
