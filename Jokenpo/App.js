3533b1633c6a1374dd834d1e2a0ff2975c28681b
atmosmaciel <atmos.mps@gmail.com>
2018-12-19 18:39:24 -0300

    Criando logica do game

--- Jokenpo/App.js
+++ Jokenpo/App.js
@@ -13,25 +13,84 @@ export default class App extends Component {
 
   constructor(props) {
     super(props)
-
-    this.state = {escolhaUsuario: ''}
+    this.state = {
+      escolhaUsuario: '',
+      escolhacomputador: '',
+      resultadoJogo: ''
+    }
   }
 
-  jokenpo(escolhaUsuario) {
-    this.setState({escolhaUsuario: escolhaUsuario})
+  jokenpo(userChoice) {
+    // gera numero aleatorio para escolha da maquina
+    var numAleatorio = Math.floor(Math.random() * 3)
+    var machineChoice = ''
+    var resultado = ''
+
+    switch (numAleatorio) {
+      case 0: machineChoice = 'Pedra'; break;
+      case 1: machineChoice = 'Papel'; break;
+      case 2: machineChoice = 'Tesoura'; break;
+    }
+
+    if (machineChoice == 'Pedra') {
+      if (userChoice == 'Pedra') {
+        resultado = 'Empate.'
+      }
+
+      if (userChoice == 'Papel') {
+        resultado = 'Usuario ganhou.'
+      }
+
+      if (userChoice == 'Tesoura') {
+        resultado = 'Computador Ganhou.'
+      }
+    }
+
+    if (machineChoice == 'Tesoura') {
+      if (userChoice == 'Tesoura') {
+        resultado = 'Empate.'
+      }
+
+      if (userChoice == 'Pedra') {
+        resultado = 'Usuario ganhou.'
+      }
+
+      if (userChoice == 'Papel') {
+        resultado = 'Computador ganhou.'
+      }
+    }
+
+    if (machineChoice == 'Papel') {
+      if (userChoice == 'Papel') {
+        resultado = 'Empate.'
+      }
+
+      if (userChoice == 'Tesoura') {
+        resultado = 'Usuario ganhou.'
+      }
+
+      if (userChoice == 'Pedra') {
+        resultado = 'Computador ganhou.'
+      }
+    }
+
+    this.setState({
+      escolhaUsuario: userChoice,
+      escolhacomputador: machineChoice,
+      resultadoJogo: resultado
+    })
   }
 
   render() {
     return (
       <View>
-       <Text>Escolha do computador</Text>
-       <Text>Escolha do User: {this.state.escolhaUsuario}</Text>
-       <Text>Resultado</Text>
+       <Text>Escolha do computador: {this.state.escolhacomputador} </Text>
+       <Text>Escolha do usuario: {this.state.escolhaUsuario}</Text>
+       <Text>Resultado: {this.state.resultadoJogo}</Text>
 
-       <Button title="pedra" onPress={ () => { this.jokenpo('pedra') } } />
-       <Button title="papel" onPress={ () => { this.jokenpo('papel') } } />
-       <Button title="tesoura" onPress={ () => { this.jokenpo('tesoura') } } />
-
+       <Button title="pedra" onPress={ () => { this.jokenpo('Pedra') } } />
+       <Button title="papel" onPress={ () => { this.jokenpo('Papel') } } />
+       <Button title="tesoura" onPress={ () => { this.jokenpo('Tesoura') } } />
       </View>
     );
   }
